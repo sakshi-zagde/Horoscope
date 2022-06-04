a784f1cb2110314e0062bb7de43c3b151c4df2e9
sakshi-zagde <89316762+sakshi-zagde@users.noreply.github.com>
2022-06-04 17:06:18 +0530

    Add files via upload

---
+++ astro_sign.py
@@ -0,0 +1,185 @@
+
+month=input("enter month")
+day=int(input("enter day"))
+if month=="march":
+    if day<21:
+        print("Pisces")
+        astro=input("Do you want to know about your horoscope")
+        if astro:
+            print("Pisces Health Today You are likely to feel fit and energetic today. Using your high level of energy to your advantage, you may take up some form of recreational activity..")
+        else:
+            print("okay")
+    else:
+        print("Aries")
+        astro=input("It will be a smooth day for you and you will get the happiness in your life. No one will able to take your happiness from you. And if you find someone who is doing so, simply stay away from them. There is no need to take too much pressure and simply work hard.")
+        if astro:
+            print("People born on this date have a zodiac Sagittarius.")
+        else:
+            print("okay")
+elif month=="april":
+    if day<19:
+        print("Aries")
+        astro=input("Do you want to know about your horoscope")
+        if astro:
+            print("It will be a smooth day for you and you will get the happiness in your life. No one will able to take your happiness from you. And if you find someone who is doing so, simply stay away from them. There is no need to take too much pressure and simply work hard.")
+        else:
+            print("okay")
+    else:
+        print("Taurus")
+        astro=input("Do you want to know about your horoscope")
+        if astro:
+            print("Don't focus too much on work and take out some time for yourself from your busy schedule. You can plan lunch or outing with your loved ones and family as spending time with them will help you today. Maintaining a positive attitude throughout the day will help you feel better.")
+        else:
+            print("okay")
+elif month=="may":
+    if day<20:
+        print("Taurus")
+        astro=input("Do you want to know about your horoscope")
+        if astro:
+            print("Don't focus too much on work and take out some time for yourself from your busy schedule. You can plan lunch or outing with your loved ones and family as spending time with them will help you today. Maintaining a positive attitude throughout the day will help you feel better.")
+        else:
+            print("okay")
+    else:
+        print("Gemini")
+        astro=input("Do you want to know about your horoscope")
+        if astro:
+            print("You have a pleasing personality. Today, you will leave quite an impression on others, especially members of the opposite gender. At work, your colleagues will need your expertise to help them through difficult tasks.")
+        else:
+            print("okay")
+elif month=="june":
+    if day<21:
+        print("Gemini")
+        astro=input("You have a pleasing personality. Today, you will leave quite an impression on others, especially members of the opposite gender. At work, your colleagues will need your expertise to help them through difficult tasks.")
+        if astro:
+            print("People born on this date have a zodiac Sagittarius.")
+        else:
+            print("okay")
+    else:
+        print("cancer")
+        astro=input("Do you want to know about your horoscope")
+        if astro:
+            print("Today, as possessive Pluto in your relationship house shoots a harsh beam to passionate Venus, you may have to face some unpleasant truths about a business or romantic partnership. Take a deep breath and initiate a frank discussion about your visions for a shared future.")
+        else:
+            print("okay")
+elif month=="july":
+    if day<22:
+        print("Cancer")
+        astro=input("Do you want to know about your horoscope")
+        if astro:
+            print("Today, as possessive Pluto in your relationship house shoots a harsh beam to passionate Venus, you may have to face some unpleasant truths about a business or romantic partnership. Take a deep breath and initiate a frank discussion about your visions for a shared future.")
+        else:
+            print("okay")
+    else:
+        print("Leo")
+        astro=input("Do you want to know about your horoscope")
+        if astro:
+            print("You might be the man of the house, but to make that house a home, you need a family. So keep that in mind, says Ganesha, and remember that your family is the foundation of your success. Make every possible effort to make your family's roots stronger.")
+        else:
+            print("okay")
+elif month=="august":
+    if day<22:
+        print("Leo")
+        astro=input("Do you want to know about your horoscope")
+        if astro:
+            print("You might be the man of the house, but to make that house a home, you need a family. So keep that in mind, says Ganesha, and remember that your family is the foundation of your success. Make every possible effort to make your family's roots stronger.")
+        else:
+            print("okay")
+    else:
+        print("Virgo")
+        astro=input("Do you want to know about your horoscope")
+        if astro:
+            print("Today will not be a too stressful day and you are suggested to stay at home and work over your life. Don't be too stressful over anything as it is a perfect day to chill. Involve yourself with the people who make you happy. You can even meet your old buddies and enjoy their company.")
+        else:
+            print("okay")
+elif month=="september":
+    if day<22:
+        print("Virgo")
+        astro=input("Do you want to know about your horoscope")
+        if astro:
+            print("Today will not be a too stressful day and you are suggested to stay at home and work over your life. Don't be too stressful over anything as it is a perfect day to chill. Involve yourself with the people who make you happy. You can even meet your old buddies and enjoy their company.")
+        else:
+            print("okay")
+    else:
+        print("Libra")
+        astro=input("Do you want to know about your horoscope")
+        if astro:
+            print("Today, you are suggested to keep calm and be happier. Don't overthink about anything and work harder. Don't be too stiff towards people. You have to keep calm and become a little hotter.")
+        else:
+            print("okay")
+elif month=="octomber":
+    if day<23:
+        print("Libra")
+        astro=input("Do you want to know about your horoscope")
+        if astro:
+            print("Today, you are suggested to keep calm and be happier. Don't overthink about anything and work harder. Don't be too stiff towards people. You have to keep calm and become a little hotter.")
+        else:
+            print("okay")
+    else:
+        print("Scorpio")
+        astro=input("Do you want to know about your horoscope")
+        if astro:
+            print("Your health is likely to be in excellent condition today. You may be free of underlying diseases. You are likely to be in a good mood, and your positive attitude may manifest itself in your physical condition")
+        else:
+            print("okay")
+elif month=="november":
+    if day<24:
+        print("Scorpio")
+        astro=input("Do you want to know about your horoscope")
+        if astro:
+            print("Your health is likely to be in excellent condition today. You may be free of underlying diseases. You are likely to be in a good mood, and your positive attitude may manifest itself in your physical condition.")
+        else:
+            print("okay")
+    else:
+        print("Sagittarius")
+        astro=input("Do you want to know about your horoscope")
+        if astro:
+            print("Today you can get everything that you were seeking for. There is no need to be affected by someone who is already trying to play pranks on you. It will be a lucky day for women and they can get everything today. Just make sure to maintain a positive and calm attitude.")
+        else:
+            print("okay")
+elif month=="december":
+    if day<25:
+        print("Sagittarius")
+        astro=input("Do you want to know about your horoscope")
+        if astro:
+            print("Today you can get everything that you were seeking for. There is no need to be affected by someone who is already trying to play pranks on you. It will be a lucky day for women and they can get everything today. Just make sure to maintain a positive and calm attitude.")
+        else:
+            print("okay")
+    else:
+        print("Capricorn")
+        astro=input("Do you want to know about your horoscope")
+        if astro:
+            print("Today your anxiety levels may remain high so make sure you take some time off to get away from issues related to work. You may be feeling a bit insecure, but this is temporary, as all things are, and this too shall pass shortly, and you will be back to being yourself again..")
+        else:
+            print("okay")
+elif month=="january":
+    if day<26:
+        print("Capricorn")
+        astro=input("Do you want to know about your horoscope")
+        if astro:
+            print("Today your anxiety levels may remain high so make sure you take some time off to get away from issues related to work. You may be feeling a bit insecure, but this is temporary, as all things are, and this too shall pass shortly, and you will be back to being yourself again.")
+        else:
+            print("okay")
+    else:
+        print("Aquarius")
+        astro=input("Do you want to know about your horoscope")
+        if astro:
+            print("It will be a lucky day for the Aquarius from the Architecture and Civil Engineering field. Today is a good day to earn money and you may find unexpected a good amount of money.")
+        else:
+            print("okay")
+elif month=="february":
+    if day<18:
+        print("Aquarius")
+        astro=input("Do you want to know about your horoscope")
+        if astro:
+            print("It will be a lucky day for the Aquarius from the Architecture and Civil Engineering field. Today is a good day to earn money and you may find unexpected a good amount of money.")
+        else:
+            print("okay")
+    else:
+        print("Pisces")
+        astro=input("Do you want to know about your horoscope")
+        if astro:
+            print("Pisces Health Today You are likely to feel fit and energetic today. Using your high level of energy to your advantage, you may take up some form of recreational activity.")
+        else:
+            print("okay")
+else:
+    print("enter months in string")
